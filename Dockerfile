0b6cf9a4669e646757980cd3d032c373d9beae59
Michaël <michael.bareyt@gmail.com>
2025-05-24 13:57:17 +0200

    update project files

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,27 @@ RUN mv $PHP_INI_DIR/php.ini-development $PHP_INI_DIR/php.ini
 
 RUN apk update && \
     apk add oniguruma-dev && \
-    docker-php-ext-install mbstring pdo pdo_mysql
+    apk add libpq-dev && \
+    docker-php-ext-install -j$(nproc) mbstring pdo pdo_mysql pgsql pdo_pgsql
 
-# RUN apk add linux-headers && \
-#     apk add $PHPIZE_DEPS && \
-#     pecl install xdebug && \
-#     docker-php-ext-enable xdebug || \
-#     echo "Can't install XDEBUG"
+# SQL Server (inspired from https://github.com/kool-dev/docker-php-sqlsrv/blob/main/7.4-nginx-sqlsrv/Dockerfile)
+RUN curl -O https://download.microsoft.com/download/fae28b9a-d880-42fd-9b98-d779f0fdd77f/msodbcsql18_18.5.1.1-1_amd64.apk && \
+    curl -O https://download.microsoft.com/download/7/6/d/76de322a-d860-4894-9945-f0cc5d6a45f8/mssql-tools18_18.4.1.1-1_amd64.apk && \
+    apk add --allow-untrusted msodbcsql18_18.5.1.1-1_amd64.apk && \
+    apk add --allow-untrusted mssql-tools18_18.4.1.1-1_amd64.apk && \
+    apk add --no-cache --virtual .persistent-deps freetds unixodbc && \
+    apk add --no-cache --virtual .build-deps $PHPIZE_DEPS freetds-dev unixodbc-dev && \
+    docker-php-ext-configure pdo_odbc --with-pdo-odbc=unixODBC,/usr && \
+    docker-php-ext-install pdo_odbc && \
+    pecl install sqlsrv pdo_sqlsrv && \
+    docker-php-ext-enable sqlsrv pdo_sqlsrv || \
+    echo "Can't install ODBC drivers"
+
+RUN apk add linux-headers && \
+    apk add $PHPIZE_DEPS && \
+    pecl install xdebug && \
+    docker-php-ext-enable xdebug || \
+    echo "Can't install XDEBUG"
 
 COPY --from=composer:latest /usr/bin/composer /usr/local/bin/
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,19 +11,78 @@ class TestCase extends OrchestraTestCase
 {
     protected function getEnvironmentSetUp($app)
     {
-        $dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env.test');
-        $dotenv->load();
-        $app['config']->set('database.default', 'test');
-        $app['config']->set('database.connections.test', [
-            'driver' => 'mysql',
+        $this->loadEnv(['.env.test', '.env']);
+        $this->setupDatabase($app, env('DB_ENGINE', 'sqlite'));
+        $app['config']->set('scout', require __DIR__ . '/../config/scout.php');
+    }
+
+    protected function loadEnv($file)
+    {
+        if (is_array($file)) {
+            foreach ($file as $f) {
+                $this->loadEnv($f);
+            }
+            return;
+        }
+        if (file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . $file)) {
+            $dotenv = Dotenv::createImmutable(dirname(__DIR__), $file);
+            $dotenv->load();
+        }
+    }
+
+    protected function setupDatabase($app, $engine = 'mysql')
+    {
+        $method = 'setup' . ucfirst($engine);
+        method_exists($this, $method) ? $this->$method($app) : $this->setupOtherSgbd($app, $engine);
+        $app['config']->set('database.default', $engine);
+    }
+
+    protected function setupSqlite($app)
+    {
+        $database = env('SQLITE_DATABASE', database_path('database.sqlite'));
+        if (file_exists($database)) {
+            unlink($database);
+        }
+        touch($database);
+
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => $database,
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => null,
+            'journal_mode' => null,
+            'synchronous' => null,
+        ]);
+    }
+
+    protected function setupMariadb($app)
+    {
+        $engine = class_exists(\Illuminate\Database\MariaDbConnection::class)
+            ? 'mariadb'
+            : 'mysql';
+        $this->setupOtherSgbd($app, $engine);
+        $app['config']->set('database.connections.mariadb', $app['config']["database.connections.$engine"]);
+    }
+
+    protected function setupSqlsrv($app)
+    {
+        $this->setupOtherSgbd($app, 'sqlsrv');
+        $app['config']->set('database.connections.sqlsrv.trust_server_certificate', true);
+    }
+
+    protected function setupOtherSgbd($app, $engine)
+    {
+        $envPrefix = strtoupper($engine);
+        $app['config']->set("database.connections.$engine", [
+            'driver' => $engine,
             'host' => env('DB_HOST'),
-            'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'port' => env("{$envPrefix}_PORT"),
+            'database' => env("{$envPrefix}_DATABASE", env('DB_DATABASE')),
+            'username' => env("{$envPrefix}_USERNAME", env('DB_USERNAME')),
+            'password' => env("{$envPrefix}_PASSWORD", env('DB_PASSWORD')),
             'prefix'   => '',
         ]);
-        $app['config']->set('scout', require __DIR__ . '/../config/scout.php');
     }
 
     protected function getPackageProviders($app)
